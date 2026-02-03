f692590ddb64d6408254c39682580ed00e8af454
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-02-03 12:03:18 +0700

    ALHAMDULILLAH APLIKASI PERTAMA GW BANG

---
+++ src/MahasiswaUtm.java
@@ -0,0 +1,12 @@
+public class MahasiswaUtm {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("KARTU IDENTITAS MAHASISWA");
+        System.out.println("==========================================");
+        System.out.println("Nama    : Muhammad Ainul Fuady");
+        System.out.println("Nim     : 240411100009");
+        System.out.println("Jurusan : Informatich Enggineering");
+        System.out.println("Status  : Mahasiswa Aktif - Semester 4");
+        System.out.println("==========================================");
+    }
+}
