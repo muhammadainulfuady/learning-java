cad5143b2eae5fccde131ddad894e13e33d00b48
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-02-03 20:08:41 +0700

    menambahkan file kalkulator di tipe data number

---
+++ src/tipe-data-number/b-floating-point-number/FloatingPointNumber.java
@@ -0,0 +1,22 @@
+public class FloatingPointNumber {
+    public static void main(String args) {
+        /*
+         * setelah kita membahas mengenai tipe data (integer-number), selanjutnya kita
+         * akan membahas mengenai floating-point-number ada 2 di antaranya adalah:
+         * 1. float
+         * - memiliki ukuran: 32 atau (4 byte)
+         * - akurasi 6 - 7 digit desimal
+         * - wajib memberikan huruf f dibelakang : 36.7f
+         * 
+         * 2. double
+         * - memiliki kapasaitas 64 bit yang setara dengan (8 byte)
+         * - akurasi 15 - 16 digit desimal
+         * - tidak perlu menambahkan huruf d di belakang
+         * 
+         * NB : jika kita menulis angka desimal dan tanpa huruf di belakangnya maka di
+         * anggap sebagai floatingNUmber (double)
+         */
+
+        // contoh soal
+    }
+}
---
+++ src/tipe-data-number/b-floating-point-number/KalkulatorSains.java
@@ -0,0 +1,10 @@
+public class KalkulatorSains {
+    public static void main(String[] args) {
+        double pi = 3.1415926535;
+        float jariJariLingkaran = 7.5F;
+        float suhuBadan = 36.6F;
+        System.out.println("Jari - jari     : " + jariJariLingkaran);
+        System.out.println("Luas lingkaran  : " + pi * jariJariLingkaran * jariJariLingkaran);
+        System.out.println("Suhu badan      : " + suhuBadan);
+    }
+}
